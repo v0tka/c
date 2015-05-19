8d4a570957ae577e1d4cfef29054b05efc4425bb
Brian Luna <brians@blackhole.cl>
2015-05-19 18:34:49 -0300

    ...

--- call_to_me.c
+++ call_to_me.c
@@ -4,36 +4,68 @@
 #include <unistd.h>
 #include <wait.h>
 
+void writer(const char* message, FILE* stream) {
+  fprintf(stream, "%s\n",message);
+  fflush(stream);
+  sleep(1);
+}
+
+void reader(FILE* stream) {
+  char buffer[1024];
+
+  while(!feof(stream) && !ferror(stream)
+        && fgets(buffer, sizeof(buffer), stream) != NULL) {
+    fputs(buffer, stdout);
+  }
+}
+
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
-  int descendencia = 1;
   pid_t pid;
+
 
   pid = fork();
 
-  if(!pid) {
+  if(pid == (pid_t)0) {
+    printf("Hijo\n");
 
-    if(descendencia < 3) {
+    int fds[2];
+    pipe(fds);
 
-      pid = fork();
 
-      if(!pid) {
-        printf("Soy hijo%d con PID=%d mi numero aleatorio es %d y el del otro proceso es \n",descendencia,getpid(),rand() % 20);
-      } else {
-        waitpid(pid,NULL,0);
-      }
+    pid = fork();
+
+    if(pid == (pid_t)0) {
+
+      FILE* stream;
+      close(fds[0]);
+
+      stream = fdopen(fds[1], "w");
+      srand(time(NULL));
+
+      char *msg = (char*)malloc(sizeof(char)*1024);
+
+      sprintf(msg, "Soy el hijo %d con PID=%d, mi numero aleatorio es %d y el del otro proceso es %d",1,getpid(),rand() % 100,getppid());
+      writer(msg,stream);
+
+      close(fds[1]);
+
+    } else {
+
+
+      FILE* stream;
+      close(fds[1]);
+      stream = fdopen(fds[0], "r");
+      reader(stream);
+      close(fds[0]);
+
     }
-
-    descendencia++;
 
   } else {
 
 
 
-    waitpid(pid, NULL, 0);
   }
-
-
 
   return 0;
 }
